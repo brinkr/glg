bb7e3418c4e668f99ca6eda482f6cdf24c1747fc
brinkr <memoever@msn.com>
2017-08-31 20:37:28 +0800

    push

---
+++ js/preventDefault.js
@@ -0,0 +1,21 @@
+
+$(function () {
+    $('.backbar').on('touchmove', function (event) {event.preventDefault();});    //把header的滑动给禁掉
+
+    // $('footer').on('touchmove', function (event) {event.preventDefault();});    //把footer的滑动给禁掉
+    
+    // //把nav的默认滚动去掉
+	// $('.tab').on('touchmove', function (event) {event.preventDefault();});	
+    
+    // /*退出提示框*/
+    // $('.weui-mask').on('touchmove', function (event) {event.preventDefault();});   
+    // $('.weui-dialog').on('touchmove', function (event) {event.preventDefault();});
+    
+    // /*数据加载中提示*/
+    // $('.weui-mask_transparent').on('touchmove', function (event) {event.preventDefault();});   
+    // $('.weui-toast').on('touchmove', function (event) {event.preventDefault();});
+    
+    // $('.add').on('touchmove', function (event) {event.preventDefault();});
+});
+
+
